89f92bf7ad952e95b191df9fa9fb4d44335397b2
exw4141 <exw4141@rit.edu>
2019-10-26 17:10:47 -0400

    Positioned arrow icons

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,4 +1,6 @@
 var app = angular.module('destress', [])
 app.controller('destressController', function($scope) {
     $scope.name = "Mason";
+    $scope.imageRootPath = "../images/rain.jpg" // should the path be from the .html file or this file
+    $scope.image = "rain.jpg";
 })
